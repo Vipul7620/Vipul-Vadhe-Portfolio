cf6ed2a84c3b8f6a7e7283fbcca94f1151cc4d58
Vipul Vadhe <vipulmilindvadhe@gmail.com>
2025-07-15 06:45:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <TypingAnimation>

--- src/components/ui/typing-animation.tsx
+++ src/components/ui/typing-animation.tsx
@@ -1,8 +1,9 @@
+
 'use client';
 
 import { useState, useEffect } from 'react';
 
-const titles = ['Graphic Designer', 'Developer', 'Innovator'];
+const titles = ['Graphic Designer', 'Developer'];
 
 const TypingAnimation = () => {
   const [index, setIndex] = useState(0);
